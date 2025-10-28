8bb69101b3d77e6614d7694b20d08b8402e992a8
Aleksandr Lundin <aslundin@yandex.by>
2025-10-28 17:52:37 +0300

    add methods edit and delete chirps and add Policy

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -23,14 +23,6 @@ class ChirpController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
     public function store(Request $request)
@@ -58,34 +50,41 @@ class ChirpController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    public function edit(Chirp $chirp)
     {
-        //
+        return view('chirps.edit', compact('chirp'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, Chirp $chirp)
     {
-        //
+
+        // Validate
+        $validated = $request->validate([
+            'message' => [
+                'required',
+                'string',
+                'max:255',
+            ],
+        ]);
+    
+        // Update
+        $chirp->update($validated);
+    
+        return redirect('/')->with('success', 'Chirp updated!');
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Chirp $chirp)
     {
-        //
+        $chirp->delete();
+ 
+        return redirect('/')->with('success', 'Chirp deleted!');
     }
 }
